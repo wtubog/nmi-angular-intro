78c936a086134ca2723259c432c1e96bed34a6e0
Wilco Tubog <wtubog@gmail.com>
2018-06-06 09:59:41 +0800

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
